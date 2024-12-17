b25cb07b81186c238b2647dedacb2b1804f9a9f9
BankNatthaphong112 <bankt661@gmail.com>
2024-12-17 10:37:31 +0700

    update

--- udf.php
+++ udf.php
@@ -3,47 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Defined Funtion</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>ทดสอบการสร้าง Funtion ทร่สร้างโดยผู้พัฒนาโปรแกรม</h1>
-    <?php
-    $name = "Natthaphon";
-    Hello($name)
-    //-----------------------------------------
-    echo "<h3>ทดสอบการใช้ Function</h3>";
+    <h1>ทดสอบการสร้าง Function ที่สร้างโดยผู้พัฒนาโปรแกรม</h1>
+    <?php 
+    $name = "Natthaphong";
+    Hello($name);
+    //----------------------------------
+    echo "<h3>ทดสอบการใช้ function</h3>";
     $a = 5;
     $b = 8;
     $c = sum($a,$b);
     echo "$a + $b = $c <br>";
-    echo "<h3>ทดสอบการใช้ Function แบบฦึกำารามิเอตอร์เป็น Reference</h3>";
+    echo "<h3>ทดสอบการใช้ function แบบมีพารามีเตอร์เป็น reference</h3>";
     $num = 2;
-    echo "Before ===> \$num = $num <br>"
+    echo "Before \$num = $num <br>";
     add_5($num);
-    echo "After ===> \$num = $num"
-    echo "<hr>"
-    //-----------------------------------------
+    add_5($num);
+    echo "After \$num = $num <br>";
+    echo "<hr>";
+    echo "<h3>ทดสอบการใช้ function แบบมีพารามีเตอร์หลายตัว</h3>";
+    $summaryNumber = sumMyNumber(1,2,3,4,5,6,7,8,9);
+    echo "ผลรวมของ (1,2,3,4,5,6,7,8,9) = $summaryNumber";
+    echo "<hr>";
+    //----------------------------------
     bye();
 
-    function add_5(&$value){
-        $value += 5;
+    function sumMyNumber(...$x){
+        $sum = 0;
+        foreach ($x as $value) {
+            $sum += $value;
+        }
+        return $sum;
     }
 
+    function add_5(&$value){
+        $value +=5 ;
+    }
+    
     function Hello($yourname){
-        echo "<h3>ผู้พัฒนาโปรแกรม</h3>";
-        echo "<h3>" .$yourname. "</h3>";
-        echo "<hr>"
+        echo "<h3>พัฒนาโปรแกรม</h3>";
+        echo "<h3>.$yourname.</h3>";
+        echo "<hr>";
     }
     function sum($x,$y){
         $z = $x + $y;
         return $z;
     }
     function bye(){
-        echo "<hr>"
-        echo "<h4>หลักสูตรวิทยาศาสตร์บันฑิต เทคโนโลยีสารสนเทศ</h4>"
-        echo "<h4>คณะวิทยาศาสตร์ มหาวิทยาลัยราชภัฏอุดรธานี</h4>"
-        echo "<hr>"
+        echo "<hr>";
+        echo "<h4>หลักสูตรเทคโนโลยีสารสนเทศ</h4>";
+        echo "<h4>คณะวิทยาศาสตร์ มหาวิทยาลัยราชภัฎอุดรธานี</h4>";
+        echo "้<hr>";
     }
+
     ?>
 </body>
 </html>
